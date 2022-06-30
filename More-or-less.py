43752d3baab7e0d99c13b0b4cbec955b3ad3c841
kevin81767 <kevin81767@gmail.com>
2022-06-30 17:15:50 +0200

    added number of trials

--- More-or-less.py
+++ More-or-less.py
@@ -3,6 +3,7 @@
 
 import os
 from random import randrange
+import time
 
 
 
@@ -10,6 +11,7 @@ the_number = randrange(1,101)
 the_number=int(the_number)   #Conversion of the number in int
 user_number=int()
 user_name=input("what's your name? ")  #The name of the user
+user_trial=1
 
 
 while user_number != the_number:  
@@ -17,14 +19,17 @@ while user_number != the_number:
        user_number=input()
        user_number=int(user_number)
        if the_number > user_number:
-           print(" It's More.")      #Au cas ou ca sere superieur
+           print(" It's More.")      #In case the number is greater
+           user_trial=user_trial+1
        elif the_number < user_number:
-               print(" It's Less.")   #Au cas ou ca sere inferieur
+               print(" It's Less.")   #In case the number is lower
+               user_trial=user_trial+1
        else:
-                   print(" Congratulation ",user_name," you find the right number")   #Au cas ou il gagne
+                   print(" Congratulation ",user_name," you found the right number. With ",user_trial," trial")   #If the user win
+                   print("")
 
 
-                   os.system("pause")
+                   time.sleep(3)
 
 
 
